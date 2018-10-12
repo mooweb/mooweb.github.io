1b73916e10cee88061a2855e81921531fc026c08
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-12 10:48:58 -0500

    Update script.js

--- oct10/joy/lab/js/script.js
+++ oct10/joy/lab/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+$('div").click{function(){
+   $(this).hide(){
+});
+  $('img').click(funtions()}
+                 $(body').css({'border-radium':'100px'});
+                 });
 });
