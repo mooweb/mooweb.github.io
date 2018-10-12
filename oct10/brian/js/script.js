440c4056fff5026c94ef8ec4d8a6106d48640277
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-12 09:53:11 -0500

    Update script.js

--- oct10/brian/js/script.js
+++ oct10/brian/js/script.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
 $('#rounded').click(function(){
   $('div').css({'border-radius':'10em'});
 });
+ $('div').click(function(){
+    $(this).fadeout(3000);
+ });
+$('img').click(function(){
+    $(this).fadeout(3000);
+ });
+  $("button").click(function(){
+    $("p").hide();
+});
+  
+  
   $('p').click(function(){
     $('#custom').css({'bacground-color':'yellow'});
 });
