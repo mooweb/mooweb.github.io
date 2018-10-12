255ac9a8c5e0bb92344c4034c27d88b39ad5f612
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-12 10:04:14 -0500

    Update script.js

--- oct10/jennifer/js/script.js
+++ oct10/jennifer/js/script.js
@@ -10,4 +10,8 @@ $(document).ready(function(){
   $('#rounded').click(function(){
     $('div').css({'border-radius':'10em'});
   });
+  
+  $('p').click(function(){
+    $('#custom').css({'background-color':'yellow'});
+                     });
 });
