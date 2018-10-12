e31cc2a818ac273cf48e718bfdb1fd717bd8cba2
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-12 10:11:56 -0500

    Update script.js

--- oct10/jordan/js/script.js
+++ oct10/jordan/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 
+  $('#rounded').click(function(){
+      $)'div').css({'border-radius':'10em'});
+  });
+  
+  $('p').click(function(){
+    $('#custom').css({'background-color':'yellow'});
+  });
+  
 });
