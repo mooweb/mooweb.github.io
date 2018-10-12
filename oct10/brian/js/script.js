8cce171a4aa8b1ad3688b6db28ecec4f4847b63b
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-12 10:02:22 -0500

    Update script.js

--- oct10/brian/js/script.js
+++ oct10/brian/js/script.js
@@ -3,7 +3,7 @@ $('#rounded').click(function(){
   $('div').css({'border-radius':'10em'});
 });
  $('div').click(function(){
-    $('this').fadeout(3000);
+    $(this).fadeout(3000);
  });
 $('img').click(function(){
     $('img').hide();
