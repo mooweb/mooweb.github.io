953e4c1485175407c4feddfac3807c31e8e352b8
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-12 09:54:05 -0500

    Update script.js

--- oct10/jennifer/js/script.js
+++ oct10/jennifer/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+  $('img').click(function(){
+      $('img').hide();
+  });
+  
+  $('div').click(function(){
+    $(this).fadeOut(3000);
+  });
+  
+  $('#rounded').click(function(){
+    $('div').css({'border-radius':'10em'});
+  });
 });
