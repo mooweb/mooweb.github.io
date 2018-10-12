6a1b8a429f968e20cacd0edae46eabbcef049fab
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-12 11:27:44 -0500

    Update script.js

--- oct10/jordan/lab/js/script.js
+++ oct10/jordan/lab/js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+  
   $('div').click(function(){
   $(this).hide()}
+  });
+  
   $('img').click(function(){
   $('body').css({'border-radius': '100px'});
   });
+
 });
