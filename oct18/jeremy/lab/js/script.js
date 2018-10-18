9bcdb766f3ae64c5f80159235b62c0e4cd2bc821
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-18 11:07:27 -0500

    Update script.js

--- oct18/jeremy/lab/js/script.js
+++ oct18/jeremy/lab/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-
+$('div').click(function(){
+  $(this).hide();
 });
+$('img').click(function(){
+  $(img).css({'border-radius':'100px'});
+});
+  
+  $('button').click(function(){
+    $('img').hide();
+    $('body').css({'background':'purple'});
+  });
+  
+});
+  
