4c622e2f07f34f0545bbccca222c0378f3eff74c
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-18 11:07:57 -0500

    Update script.js

--- oct18/lori/lab/js/script.js
+++ oct18/lori/lab/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
  });
   
   $('img').click(function(){
-  $('body').css({'border-radius':'100px'});
+  $('img').css({'border-radius':'100px'});
   });
   
   $('button').click(function(){
