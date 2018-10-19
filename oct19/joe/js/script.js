e8e75e4dce96b053cc0f3b5b7b813433966382aa
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-19 13:20:00 -0500

    Update script.js

--- oct19/joe/js/script.js
+++ oct19/joe/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
  $('#rounded'.click(function(){
       $('div').css({'border-radius':'10em'});
  });
-   $('p').click(function(){
-     $('#custom').css({'background-color':'blue'});
+ $('p').click(function(){
+   $('#custom').css({'background-color':'blue'});
    });
 )}:
