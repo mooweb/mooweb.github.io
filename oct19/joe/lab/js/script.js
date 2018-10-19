d9dbec977e8f10e0fe700b61a677916f6682b4e3
mooweb <42480578+mooweb@users.noreply.github.com>
2018-10-19 14:13:39 -0500

    Update script.js

--- oct19/joe/lab/js/script.js
+++ oct19/joe/lab/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('div').click(function(){
     $(this).hide();
   });
-  $('img').click(function(){
-    $('img').css({'border_radius':'100px'});
+  $('#rounded').click(function(){
+    $('div').css({'border-radius':'10em'});
   });
 });
