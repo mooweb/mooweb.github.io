430e0c1eaf96ae38deee28d30ae3e0fd8d931dc9
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 10:38:20 -0600

    Update script.js

--- nov7/grant/lab/js/script.js
+++ nov7/grant/lab/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+  $('div').click(function(){
+   $(this.hide();
 });
+    $('img').click(function(){
+      $('body').css({'border-radius': '100px'});
+    });
+  });
