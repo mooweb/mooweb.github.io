6f19669e62b94167b31de98e0d22a03af204b270
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 10:45:52 -0600

    Update script.js

--- nov7/bob/lab/js/script.js
+++ nov7/bob/lab/js/script.js
@@ -3,7 +3,7 @@ $('div').click(function(){
 $(this).hide();
 });
   $('img').click(function)(){
-    $('img').css({border-radius':'100px'});
+    $('img').css({'border-radius':'100px'});
  });
     $('button').click(function(){
       $('body').css({'backround':'red'});
