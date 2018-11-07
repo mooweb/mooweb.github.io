86ae9f7fcb89690df95cb00397323b44efd59593
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 10:20:15 -0600

    Update script.js

--- nov7/karen/lab/js/script.js
+++ nov7/karen/lab/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+  $('div').click(function(){
+  $(this).hide(){
+    });
+  $('img').click(function(){
+  $('body').css({'border-radius': 100px'});
+                });                  
+                     
 });
