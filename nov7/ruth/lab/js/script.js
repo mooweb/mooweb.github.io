0d1d0fa4285c288e313fae1fa1bec51d1044305c
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 10:43:38 -0600

    Update script.js

--- nov7/ruth/lab/js/script.js
+++ nov7/ruth/lab/js/script.js
@@ -5,4 +5,7 @@ $(document).ready(function(){
 $('img').click(function(){
 $('img').css({'border-radius':'100px'});
 });
+  $('button').click(function(){
+    $('body').css({'background':'red'});
+  });
 });
