964721ea67dbbc290d29fb05e521c16803e12f7d
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 10:47:00 -0600

    Update script.js

--- nov7/ron/lab/js/script.js
+++ nov7/ron/lab/js/script.js
@@ -4,9 +4,12 @@ $(document).ready(function(){
     });
     
     $('img').click(function(){
-      $('body').css({'border-radius':'100px'});
+      $('img').css({'border-radius':'100px'});
     });
   
+    $('button').click(function(){
+     $('body').css({'background':'red'});
+   });
   
   
   
