96bd1eecbb7930c0e40cf0c03609fd2932533357
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 09:41:42 -0600

    Update script.js

--- nov7/bruce/js/script.js
+++ nov7/bruce/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  $('#hide')..click(function(){
+  $('#hide').click(function(){
     $('img').hide();
   });
   $('#animate').click(function(){
-    $('div').css({boarder-radus':'100px'});
+    $('div').css({'border-radius':'100px'});
   });
 });
