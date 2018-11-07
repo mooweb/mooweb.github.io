b74c934fe9240a1773308ebd0f149c6a72744c9e
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 09:39:34 -0600

    Update script.js

--- nov7/melissa/js/script.js
+++ nov7/melissa/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $('#hide').function(){
+    $('img').hide();
+  });
+  
+  $('animate').click(function(){
+    $('div').css({'border-radius':'100px'});
+  });
+  
 });
