d720fad4299aac9e7f6b1f62a324dea74545d698
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 09:40:36 -0600

    Update script.js

--- nov7/karen/js/script.js
+++ nov7/karen/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-
+  $('#hide').click(function(){
+    $('img').hide()'
+  });
+  
+  $('#animate').click(function(){
+    $('div').css({'border-radious':'100px'});
+  });             
+    
 });
+
