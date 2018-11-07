3430b2bfbb4fa1d019c3281f26fdaf20750110a7
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 10:16:29 -0600

    Update script.js

--- nov7/ron/lab/js/script.js
+++ nov7/ron/lab/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+  $('div').click(function(){
+    $(this).hide(){
+    });
+    
+    $('img').click(function(){
+      $('body').css({'border-radius':'100px'});
+    });
+  
+  
+  
+  
 });
