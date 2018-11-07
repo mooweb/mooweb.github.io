702c1c8faf325d52b467aa5a74f64500d35bdae8
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 09:39:20 -0600

    Update script.js

--- nov7/bruce/js/script.js
+++ nov7/bruce/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+  $('#hide')..click(function(){
+    $('img').hide();
+  });
+  $('#animate').click(function(){
+    $('div').css({boarder-radus':'100px'});
+  });
 });
