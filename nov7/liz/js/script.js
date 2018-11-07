6bf59d2d1da47b488bf4d0a125a5c543d9eecf75
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 09:41:00 -0600

    Update script.js

--- nov7/liz/js/script.js
+++ nov7/liz/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-
+  $('#hide').click(function(){
+    $('img').hide();
+  });
+  
+  $('#animate').click(function(){
+    $('div').css({'border-radius':'100px'});
+  });
+  
+  $('p').click(function(){
+    $('#custom').css({background-color':'yellow'});
+  });
+    
 });
