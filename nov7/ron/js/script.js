30588bf68593847b591badbffa4553e01865c850
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 09:38:43 -0600

    Update script.js

--- nov7/ron/js/script.js
+++ nov7/ron/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+  $('#hide').click(function(){
+    $('img').hide();
+  });
+  
+  
+  $('#animate').click(function(){
+    $('div').css({'border-radius':'100px'});
+  }); 
+    
+    
+    
 });
