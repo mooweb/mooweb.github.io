cf757f4a062a27122c5d9ce622c40ac9a38d4240
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-07 10:45:28 -0600

    Update script.js

--- nov7/bob/lab/js/script.js
+++ nov7/bob/lab/js/script.js
@@ -4,7 +4,7 @@ $(this).hide();
 });
   $('img').click(function)(){
     $('img').css({border-radius':'100px'});
-                 });
+ });
     $('button').click(function(){
       $('body').css({'backround':'red'});
    });
