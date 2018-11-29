bd9d9c3585b8de6a0cd8f5ff566cb8a6f20c102c
mooweb <42480578+mooweb@users.noreply.github.com>
2018-11-29 14:49:52 -0600

    Update script.js

--- nov29/shonna/lab/js/script.js
+++ nov29/shonna/lab/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+  $('div').click(function(){
+    $(this).slideUp();
+  });
+  
+  $('img').click(function(){
+    $('img').css({'border-radius':'10em'});
+  });
 });
