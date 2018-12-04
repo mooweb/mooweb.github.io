d5e8f1501df5c1cc0df6c34ce4487e6c582fe993
mooweb <42480578+mooweb@users.noreply.github.com>
2018-12-04 14:18:09 -0600

    Update script.js

--- dec4/sean/lab/js/script.js
+++ dec4/sean/lab/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+  $('div').click(function(){
+    $(this).hide(){
+    });
+    $('img').click(function(){
+      $('img').css({'border-radius':'100px'});
+    });
 });
