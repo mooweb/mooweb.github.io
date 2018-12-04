a9cb186c79ec7182fe8f8f156323164075408a5b
mooweb <42480578+mooweb@users.noreply.github.com>
2018-12-04 13:48:40 -0600

    Update script.js

--- dec4/sean/js/script.js
+++ dec4/sean/js/script.js
@@ -3,6 +3,6 @@ $('button').click(function(){
   $('div').css({'border-radius':'10em'});
 });
 $('p').click(function(){
-  $('#custom').css({'background':'yellow'});
+  $('#custom').css({'background':'green'});
 });
 });
