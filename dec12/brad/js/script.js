319b06c7e23e5f21e5dbd1702259700194f6c7b6
mooweb <42480578+mooweb@users.noreply.github.com>
2018-12-12 14:43:59 -0600

    Update script.js

--- dec12/brad/js/script.js
+++ dec12/brad/js/script.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
   $('img').fadeIn(3000);
   $('button').click(function(){
     $('img').css({'border-radius':'10em'});
-  }); 
+  });
+  $('body).click(function)(){
+    $('body).css({'background':'black'});
+    });
 });
