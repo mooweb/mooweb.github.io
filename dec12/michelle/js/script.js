cf905c146d686350e7f84d533f656b573bdb4dcf
mooweb <42480578+mooweb@users.noreply.github.com>
2018-12-12 14:28:19 -0600

    Update script.js

--- dec12/michelle/js/script.js
+++ dec12/michelle/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
  $ ('img').fadeIn('slow');
  $('rounded').click(function(){
- $('img'.css({'border-radius','10em'});
+ $('img').css({'border-radius','10em'});
  });
 });
