87ab69e9335a7d2abb551a265cb01841a4950b1a
mooweb <42480578+mooweb@users.noreply.github.com>
2018-12-12 14:49:42 -0600

    Update script.js

--- dec12/michelle/js/script.js
+++ dec12/michelle/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
  $('img').css({'border-radius':'10em'});
  });
 $('body').click(function(){
- $('body').css({'background':'red'});
+ $('body').css({'background':'blue'});
 });
 });
 
