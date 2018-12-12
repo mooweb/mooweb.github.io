34b02fecc8d389ebcb72aa2dda61869566c79b72
mooweb <42480578+mooweb@users.noreply.github.com>
2018-12-12 14:45:19 -0600

    Update script.js

--- dec12/shonna/js/script.js
+++ dec12/shonna/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+  $('img').fadeIn(3000);
+  $('button').click(function(){
+    $('img').css({'border-radius':'10em'});
+  });
+  $('body').click(function)(){
+    $('body').css({'background':'red'});
+  });
 });
