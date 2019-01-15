41e3f4b4ba270bc5a06b70820da62e7a794762bf
mooweb <42480578+mooweb@users.noreply.github.com>
2019-01-15 14:20:42 -0600

    Update script.js

--- jan15/shonna/js/script.js
+++ jan15/shonna/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+  $('#rounded').click(function(){
+    $('div').css({'border-radius':'10em'});
+  });
+  
+  $('#animate').click(function(){
+    $('p').css({'background':'yellow'});
+  });
+  
+  $('#hide').click(function(){
+    $('img').hide();
+  });
 });
