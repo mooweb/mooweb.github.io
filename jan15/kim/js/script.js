a6474ee6854fd74665f28246d368b1354c6ca1c3
mooweb <42480578+mooweb@users.noreply.github.com>
2019-01-15 14:19:06 -0600

    Update script.js

--- jan15/kim/js/script.js
+++ jan15/kim/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+  $('#rounded').click(function(){
+    $('div').css({'border-radius':'10em'});
+  });
+  
+  $('#animate').click(function(){
+    $('p').css({'background':'purple'});
+  });
+  
+  $('#hide').click(function(){
+    $('img').hide();
+  });
 });
