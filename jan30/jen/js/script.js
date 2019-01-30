75e724b4c6d7ae9bdc586a247cfe8d0c151a246e
mooweb <42480578+mooweb@users.noreply.github.com>
2019-01-30 14:44:30 -0600

    Update script.js

--- jan30/jen/js/script.js
+++ jan30/jen/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 $('#rounded').click(function(){
-  $('div).css({border-radius':'10em'});
+  $('div').css({'border-radius':'10em'});
 });
 $('p').click(function(){
   $('#custom').css({'background':'yellow'});
 });
+  $('img').fadeOut();
 });
