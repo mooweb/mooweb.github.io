afa8cf17dfcdf465f7e6d3c645e27471c3ce31ff
mooweb <42480578+mooweb@users.noreply.github.com>
2019-01-30 14:43:53 -0600

    Update script.js

--- jan30/tony/js/script.js
+++ jan30/tony/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
- $('#rounded).click(function(){
-   $('dic').css({'border-radius':'10em'});
+ $('#rounded').click(function(){
+   $('div').css({'border-radius':'10em'});
 });
 $('p').click(function(){
   $('#custom').css({'background':'yellow'})
