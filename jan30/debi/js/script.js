cedf68ca82a120dd1568084d3438ae7712073a19
mooweb <42480578+mooweb@users.noreply.github.com>
2019-01-30 14:45:10 -0600

    Update script.js

--- jan30/debi/js/script.js
+++ jan30/debi/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+$('#rounded').click(function()[
+  $('div').css({'border-radius':'10em'});
+  });
+  4('p')click(function(){
+  $(3custom').css({'background':'yellow'})
+  ]);
 });
