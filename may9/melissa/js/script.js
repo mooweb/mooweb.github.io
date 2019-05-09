f588f9c08b0c6a387cf80811ce6ca00b12177a15
mooweb <42480578+mooweb@users.noreply.github.com>
2019-05-09 15:43:29 -0500

    Update script.js

--- may9/melissa/js/script.js
+++ may9/melissa/js/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
   $('#rounded').click(function(){
-    $('div').css({'border-radius':'10em'});
+    $('div').css({border-radius':'10em'});
   });
- $('p').click(function(){
-   $('#custom').css({'background':'pink'});
- });
+  $('p').click(function(){
+    $('#custom').css({backgruond':'pink'});
+  });
 $('div').click(function(){
 $(this).hide();
-    })
-  $('img').fadeIn(3);
+});
+  
+  
+  $('img').fadeIn(300);
 
 });
