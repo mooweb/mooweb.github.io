8a64f8e1f1d07a29996c1fa9fd3c74bf09959241
mooweb <42480578+mooweb@users.noreply.github.com>
2019-05-09 15:23:03 -0500

    Update script.js

--- may9/ben/js/script.js
+++ may9/ben/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#rounded').click(fuction(){
-    $('div').css({'boarder-radius':'10em;'})
+    $('div').css({'boarder-radius':'10em'});
 });
 $('p').click(function(){
   $('#custom').css({'background':pink'});
