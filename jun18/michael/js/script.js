b8b617c1c14a9ac64f4448e917cfbe363cfdb057
mooweb <42480578+mooweb@users.noreply.github.com>
2019-06-18 15:41:47 -0500

    Update script.js

--- jun18/michael/js/script.js
+++ jun18/michael/js/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 $('img').fadeIn('slow');
-   $('button').click(function(){
+   $('#hide').click(function(){
      $('img').hide();
    });
    $('rounded').click(function(){
       $('div').css({'border-radius':'10em'});
 });
+   $('animate').click(function(){
+      $('#custom').css({'background':'pink'});
 
-
+   });
+   
+});
 
