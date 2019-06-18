0a207f8166773a75c359864f736fb27da9ea4fd7
mooweb <42480578+mooweb@users.noreply.github.com>
2019-06-18 15:40:34 -0500

    Update script.js

--- jun18/shonna/js/script.js
+++ jun18/shonna/js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-$('img').fadeIn('slow');
+
+  $('img').fadeIn('slow');
+  
   $('button').click(function(){
     $('img').hide();
   });
+  
+  $('#rounded').click(function(){
+    $('div').css({'border-radius':'10em'});
+  });
+  
+  $('#animate').click(function(){
+    $('#custom').css({'background':'pink'});
+  });
+  
 });
