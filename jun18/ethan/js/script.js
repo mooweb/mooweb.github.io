1e1860ad68f6f5fb66a795463821836f8d4e9390
mooweb <42480578+mooweb@users.noreply.github.com>
2019-06-18 15:40:14 -0500

    Update script.js

--- jun18/ethan/js/script.js
+++ jun18/ethan/js/script.js
@@ -5,4 +5,14 @@ $('img').fadeIn('slow');
 $('button').click(function() {
     $('img').hide();
 });
+  
+ $('#rounded').click(function() {
+   $('div').css({'border-radius':'10em'});
+   
+ });
+  
+  
+$('#animate').click(function() {
+  $('#custom').css({'background':'pink'});
+               
 });
