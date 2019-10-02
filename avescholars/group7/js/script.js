8fa0363192a82ac8b6ff07979358cc6dc5f7497c
mooweb <42480578+mooweb@users.noreply.github.com>
2019-10-02 10:44:07 -0500

    Update script.js

--- avescholars/group7/js/script.js
+++ avescholars/group7/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
  /*'#rounded').click(function(){
   $('div').css({'border-radius':'*/
 $('hide').click(function(){
-    $('#img).hide();
+    $('#img').hide();
 });
 });
       
